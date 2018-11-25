5edaa7b4e8115a4b26f262dc0bd894ff3506d857
Jackson5551 <bingham.jackson49@gmail.com>
2018-11-24 17:26:22 -0700

    Update 2

--- ProcssMan/ProcssMan/Program.cs
+++ ProcssMan/ProcssMan/Program.cs
@@ -13,23 +13,40 @@ namespace ProcssMan
         static void Main(string[] args)
         {
             Console.Title = "Running Processess";
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
             int counter = 1;
             int procssid;
+            string processName;
             Process[] processlist = Process.GetProcesses();
 
-            foreach (Process theprocess in processlist)
+            while (counter == 1)
             {
-                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+                foreach (Process theprocess in processlist)
+                {
+                    Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+                }
+                try
+                {
+                    Console.WriteLine("\nEnter the name of the process you wold like to kill and then hit enter: ");
+                    Console.Read();
+                    processName = Console.ReadLine();
+                    KillProcess(processName);
+                    Console.Clear();
+                    Console.WriteLine("Process killed.");
+                    Console.Read();
+                }catch(Exception x)
+                {
+                    Console.WriteLine(x.Message);
+                }
             }
-            
-            Console.WriteLine("\nEnter the name of the process you wold like to kill: ");
-            Console.Read();
-            procssid = Convert.ToInt32(Console.ReadLine());
-            Process p = Process.GetProcessById(procssid);
-            p.Kill();
-            Console.Read();
+        }
+        public static void KillProcess(string name)
+        {
+            foreach (var process in Process.GetProcessesByName(name))
+            {
+                process.Kill();
+            }
         }
     }
 }
